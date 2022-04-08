fc6e88700658ceb5befa478fc7f39820e8bb0937
Efstathios Charitos <stathischaritos@gmail.com>
2022-04-08 09:22:11 +0100

    move tests

--- test/test_volumes.py
+++ test/test_volumes.py
@@ -1,5 +1,6 @@
-from kubeluigi.volumes import BlobStorageVolume
+from kubeluigi.volumes import BlobStorageVolume, EphemeralVolume
 from test.test_kubernetes_job_task import DummyTask
+from kubeluigi.k8s import attach_volume_to_spec
 
 
 def test_volumes():
@@ -16,5 +17,52 @@ def test_volumes():
     updated_spec = t._attach_volumes_to_spec(spec)
     # it should have two volumes:
     # (i) default cache volume  and the v
+    assert len(updated_spec["volumes"]) == 1
+    assert len(updated_spec["containers"][0]["volume_mounts"]) == 1
+
+
+def test_ephemeral_volumes():
+    v = EphemeralVolume(10)
+    v2 = EphemeralVolume(10)
+    assert v.volume_hash() == v2.volume_hash()
+
+    vol_spec = v.pod_volume_spec()
+    mnt_spec = v.pod_mount_spec()
+    assert vol_spec["volumes"][0]["name"] == mnt_spec["volume_mounts"][0]["name"]
+    assert mnt_spec["volume_mounts"][0]["mountPath"] == "/mnt/data"
+    assert (
+        vol_spec["volumes"][0]["ephemeral"]["volumeClaimTemplate"]["spec"]["resources"][
+            "requests"
+        ]["storage"]
+        == "10Gi"
+    )
+
+    v3 = EphemeralVolume(10, "/mnt/other")
+    assert v3.pod_mount_spec()["volume_mounts"][0]["mountPath"] == "/mnt/other"
+
+
+def test_blob_storage_volumes():
+    v = BlobStorageVolume("my_account", "my_container")
+    assert v.secret_name() == "storage-sas-my_account"
+
+    v2 = BlobStorageVolume("my_account", "my_container")
+    assert v.volume_hash() == v2.volume_hash()
+
+    vol_spec = v.pod_volume_spec()
+    mnt_spec = v.pod_mount_spec()
+    assert vol_spec["volumes"][0]["csi"]["driver"] == "blob.csi.azure.com"
+    assert (
+        vol_spec["volumes"][0]["csi"]["volumeAttributes"]["containerName"]
+        == "my_container"
+    )
+
+    assert mnt_spec["volume_mounts"][0]["name"] == vol_spec["volumes"][0]["name"]
+    assert mnt_spec["volume_mounts"][0]["mountPath"] == "/mnt/my_account/my_container"
+
+
+def test_attach_volume_to_spec():
+    dummy_spec = {"containers": [{"volume_mounts": []}], "volumes": []}
+    v = BlobStorageVolume("my_account", "my_container")
+    updated_spec = attach_volume_to_spec(dummy_spec, v)
     assert len(updated_spec["volumes"]) == 1
     assert len(updated_spec["containers"][0]["volume_mounts"]) == 1
